936e3ff7cf856b5101421e664bdbd869341ab6fb
kamar2000 <mr.kamer2000@gmail.com>
2022-05-17 12:49:29 +0200

    QAMER

---
+++ lib/qamer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class QAMER extends StatelessWidget {
+  const QAMER({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
